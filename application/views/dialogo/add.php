c43b0f97d32bc90394d4ff5d75d66a3cb2817d32
Angelo Mendes <contato.angelomendes@gmail.com>
2018-08-31 17:34:53 -0300

    imagemFrase add

--- application/views/dialogo/add.php
+++ application/views/dialogo/add.php
@@ -30,3 +30,4 @@
     </div>
 
 <?php echo form_close(); ?>
+
--- application/views/imagemfrase/add.php
+++ application/views/imagemfrase/add.php
@@ -1,7 +1,7 @@
 <?php echo form_open('imagemfrase/add',array("class"=>"form-horizontal")); ?>
 
 	<div class="form-group">
-		<label for="titulo" class="col-md-4 control-label">Titulo</label>
+		<label for="titulo" class="col-md-4 control-label">Título</label>
 		<div class="col-md-8">
 			<input type="text" name="titulo" value="<?php echo $this->input->post('titulo'); ?>" class="form-control" id="titulo" />
 		</div>
@@ -19,7 +19,7 @@
 		</div>
 	</div>
 	<div class="form-group">
-		<label for="descricao" class="col-md-4 control-label">Descricao</label>
+		<label for="descricao" class="col-md-4 control-label">Descrição</label>
 		<div class="col-md-8">
 			<input type="text" name="descricao" value="<?php echo $this->input->post('descricao'); ?>" class="form-control" id="descricao" />
 		</div>
@@ -78,19 +78,7 @@
 			<input type="text" name="fraseCorreta" value="<?php echo $this->input->post('fraseCorreta'); ?>" class="form-control" id="fraseCorreta" />
 		</div>
 	</div>
-	<div class="form-group">
-		<label for="idLicao" class="col-md-4 control-label">IdLicao</label>
-		<div class="col-md-8">
-			<input type="text" name="idLicao" value="<?php echo $this->input->post('idLicao'); ?>" class="form-control" id="idLicao" />
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="ordem" class="col-md-4 control-label">Ordem</label>
-		<div class="col-md-8">
-			<input type="text" name="ordem" value="<?php echo $this->input->post('ordem'); ?>" class="form-control" id="ordem" />
-		</div>
-	</div>
-	
+
 	<div class="form-group">
 		<div class="col-sm-offset-4 col-sm-8">
 			<button type="submit" class="btn btn-success">Save</button>
