e39cff06a956e9e9c9b0783c783187f3e4ec0676
Angelo Mendes <contato.angelomendes@gmail.com>
2018-09-11 16:56:25 -0300

    correcao pergunta/truefalse

--- application/views/truefalse/truefalseResponder.php
+++ application/views/truefalse/truefalseResponder.php
@@ -1,5 +1,5 @@
 <?php echo form_open('resposta/addTrueFalse', array("class" => "form-horizontal")); ?>
-    <input type="hidden" name="trueFalse" value="<?=htmlspecialchars(json_encode($trueFalse));?>"/>
+    <input type="hidden" name="trueFalse" value="<?=htmlspecialchars(json_encode($truefalse));?>"/>
     <div>
         <?php
         /*LISTAR PERGUNTAS DE UMA LIÇÃO
@@ -15,32 +15,33 @@
         $data_inicio = $data_inicio->format('Y/m/d H:i:s');
         ?>
 
-        <span> <?php echo $trueFalse->titulo; ?></span><br>
-        <span> <?php echo $trueFalse->descricao; ?></span><br>
+        <span> <?php echo $truefalse->titulo; ?></span><br>
+        <span> <?php echo $truefalse->descricao; ?></span><br>
         <span>
-                    <?php if ($trueFalse->imagem != "" and $trueFalse->imagem != 'error') { ?>
-                        <img src="<?php echo $trueFalse->imagem; ?>" height="100px">
+                    <?php if ($truefalse->imagem != "" and $truefalse->imagem != 'error') { ?>
+                        <img src="<?php echo $truefalse->imagem; ?>" height="100px">
                     <?php } ?>
         </span>
         <span>
-        <?php if(!empty($trueFalse->video) and $trueFalse->video!='error') { ?>
+        <?php if(!empty($truefalse->video) and $truefalse->video!='error') { ?>
             <video id="video" class="" width="240" height="160" controls>
-                    <source src="<?= $trueFalse->video ?>" type='video/<?= explode('.', $trueFalse->video)[1]; ?>'>
+                    <source src="<?= $truefalse->video ?>" type='video/<?= explode('.', $truefalse->video)[1]; ?>'>
                 </video>
         <?php } ?>
         </span>
 
         <br>
-        <? $i=1?>
-        <?php foreach ($trueFalseFrases as $trueFalseFrase) { ?>
-            <span> <?php echo $trueFalseFrase->descricao; ?></span><br>
-            <input type="radio" name=<?php echo "resposta".$i; ?> id=<?php echo "resposta1[".$i."]"; ?> value="<?php echo $trueFalseFrase->opcao1; ?>"> <label
-                for=<?php echo "resposta1[".$i."]"; ?>><?php echo $trueFalseFrase->opcao1; ?></label> <br>
-            <input type="radio" name=<?php echo "resposta".$i; ?> id=<?php echo "resposta2[".$i."]"; ?> value="<?php echo $trueFalseFrase->opcao2; ?>"> <label
-                for=<?php echo "resposta2[".$i."]"; ?>><?php echo $trueFalseFrase->opcao2; ?></label> <br>
+        <?php $i=1; $j=0;?>
+        <?php $truefalse = get_object_vars($truefalse);?>
+        <?php while (isset($truefalse['frase'.$i])) { ?>
+
+            <input type="radio" name=<?php echo "resposta".$i; ?> id=<?php echo "resposta1[".$i."]"; ?> value="<?= $i/2==0? $truefalse['frase'.$i][$j]['opcaoFalse'] : $truefalse['frase'.$i][$j]['opcaoTrue']; ?>"> <label
+                for=<?php echo "resposta1[".$i."]"; ?>><?= $i/2==0? $truefalse['frase'.$i][$j]['opcaoTrue'] : $truefalse['frase'.$i][$j]['opcaoFalse']; ?></label> <br>
+            <input type="radio" name=<?php echo "resposta".$i; ?> id=<?php echo "resposta2[".$i."]"; ?> value="<?= $i/2==0? $truefalse['frase'.$i][$j]['opcaoTrue'] : $truefalse['frase'.$i][$j]['opcaoFalse']; ?>"> <label
+                for=<?php echo "resposta2[".$i."]"; ?>><?= $i/2==0? $truefalse['frase'.$i][$j]['opcaoFalse'] : $truefalse['frase'.$i][$j]['opcaoTrue']; ?></label> <br>
              <input type="hidden" name=<?php echo "respostaCorreta".$i; ?>
-            value="<?php echo $trueFalseFrase->opcaoCorreta; ?>">  <br>
-        <?php } ?>
+            value="<?php echo $truefalse['frase'.$i][$j]['opcaoCorreta']; ?>">  <br>
+        <?php $i++;} ?>
         <input type="hidden" name="data" value="<?=$data_inicio; ?>">
         <div class="form-group">
             <div class="col-sm-offset-4 col-sm-8 ">
