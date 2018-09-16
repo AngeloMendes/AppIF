21e77b80f1c8574057cf98d5f893858747b672b2
Icaro Alvarenga <icaro_alvarenga@hotmail.com>
2018-09-16 14:39:39 -0300

    login responsivo img

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,12 +1,15 @@
 <?php echo form_open('usuario/login', array("class" => "form-horizontal")); ?>
 <link rel="stylesheet" href="<?php echo base_url(); ?>application/views/css/style.css" type="text/css"/>
 
+<div align="center" class="form-add">
 
-<div style="height: auto; width: 35%; margin-top: 10%;  " class="panel panel-default container-fluid">
-    <div class="panel-body">
+<div style="height: auto; width: 35%; margin-top: 10%;  "  class="panel panel-default container-fluid  hidden-xs hidden-sm">
+    <div class="panel-body  ">
+
         <div>
-
-            <img src="<?php echo base_url();?>application/midias/ifetlogo.png" height="200"  alt="">
+            <div >
+            <img class="img-responsive" src="<?php echo base_url();?>application/midias/ifetlogo.png" height=""  alt="">
+            </div>
             <h2 class="form-signin-heading">Digite seu nome</h2>
             <div>
                 <input type="text" id="nome" name="nome" value="<?php echo $this->input->post('nome'); ?>"
@@ -22,8 +25,24 @@
     </div>
 
 </div>
+<div class="panel-body  hidden-md hidden-lg ">
 
+    <div>
 
+        <img src="<?php echo base_url();?>application/midias/ifetlogo.png" height="200"  alt="">
+        <h2 class="form-signin-heading">Digite seu nome</h2>
+        <div>
+            <input type="text" id="nome" name="nome" value="<?php echo $this->input->post('nome'); ?>"
+                   class="form-control" placeholder="Nome">
+        </div>
+        <div style="margin-top:5%">
+            <?php echo $msg; ?>
+        </div>
+        <br>
+        <button type="submit" name='login' value="login" class="btn btn-success">Entrar</button>
+        <button type="submit" name='cadastrar' value="cadastrar" class="btn btn-primary">Cadastrar</button>
+    </div>
+</div></div>
 <?php echo form_close(); ?>
 
 <style>
