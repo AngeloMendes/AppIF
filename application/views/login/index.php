ef8a9cacd022107d1805c161b16b6afaf3a56c6a
Angelo Mendes <contato.angelomendes@gmail.com>
2018-09-16 15:00:21 -0300

    fix login

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -3,46 +3,32 @@
 
 <div align="center" class="form-add">
 
-<div style="height: auto; width: 35%; margin-top: 10%;  "  class="panel panel-default container-fluid  hidden-xs hidden-sm">
-    <div class="panel-body  ">
+    <div style="height: auto; width: 35%; margin-top: 10%;  "
+         class="panel panel-default container-fluid  hidden-xs hidden-sm">
+        <div class="panel-body  ">
 
-        <div>
-            <div >
-            <img class="img-responsive" src="<?php echo base_url();?>application/midias/ifetlogo.png" height=""  alt="">
-            </div>
-            <h2 class="form-signin-heading">Digite seu nome</h2>
             <div>
-                <input type="text" id="nome" name="nome" value="<?php echo $this->input->post('nome'); ?>"
-                       class="form-control" placeholder="Nome">
+                <div>
+                    <img class="img-responsive" src="<?php echo base_url(); ?>application/midias/ifetlogo.png" height=""
+                         alt="">
+                </div>
+                <h2 class="form-signin-heading">Digite seu nome</h2>
+                <div>
+                    <input type="text" id="nome" name="nome" value="<?php echo $this->input->post('nome'); ?>"
+                           class="form-control" placeholder="Nome">
+                </div>
+                <div style="margin-top:5%">
+                    <?php echo $msg; ?>
+                </div>
+                <br>
+                <button type="submit" name='login' value="login" class="btn btn-success">Entrar</button>
+                <button type="submit" name='cadastrar' value="cadastrar" class="btn btn-primary">Cadastrar</button>
             </div>
-            <div style="margin-top:5%">
-                <?php echo $msg; ?>
-            </div>
-            <br>
-            <button type="submit" name='login' value="login" class="btn btn-success">Entrar</button>
-            <button type="submit" name='cadastrar' value="cadastrar" class="btn btn-primary">Cadastrar</button>
         </div>
+
     </div>
 
 </div>
-<div class="panel-body  hidden-md hidden-lg ">
-
-    <div>
-
-        <img src="<?php echo base_url();?>application/midias/ifetlogo.png" height="200"  alt="">
-        <h2 class="form-signin-heading">Digite seu nome</h2>
-        <div>
-            <input type="text" id="nome" name="nome" value="<?php echo $this->input->post('nome'); ?>"
-                   class="form-control" placeholder="Nome">
-        </div>
-        <div style="margin-top:5%">
-            <?php echo $msg; ?>
-        </div>
-        <br>
-        <button type="submit" name='login' value="login" class="btn btn-success">Entrar</button>
-        <button type="submit" name='cadastrar' value="cadastrar" class="btn btn-primary">Cadastrar</button>
-    </div>
-</div></div>
 <?php echo form_close(); ?>
 
 <style>
